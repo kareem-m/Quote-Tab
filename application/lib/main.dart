a14ef4b3decc2ba2059d42090e6408dcbc85d1f7
Amr Mohamed <amr.81@outlook.com>
2025-09-25 10:20:20 +0300

    Change App Name

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Quote Tab Todo',
       theme: ThemeData(
         textTheme: GoogleFonts.cairoTextTheme()
